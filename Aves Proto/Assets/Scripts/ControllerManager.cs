649cce4e12d0e58ffce8d7972fdfa6e9943697cb
Cramena <allezcheznice@gmail.com>
2018-02-12 18:51:44 +0100

    First commit

---
+++ Aves Proto/Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ControllerManager : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public float rotationSpeed = 100.0f;
+
+	CinemachineFreeLook cam;
+
+    public float smooth = 4.0f;
+
+    [HideInInspector]
+    public float roll, yaw, pitch; //Inputs for roll, yaw, and pitch, taken from Unity's input system.
+
+    [SerializeField]
+    float eulerAngX;
+    [SerializeField]
+    float eulerAngY;
+    [SerializeField]
+    float eulerAngZ;
+
+
+    float myFieldOfView;
+
+    void Start ()
+    {
+        myFieldOfView = 35.0f;
+		cam = GetComponent<CinemachineFreeLook> ();
+    }
+
+
+
+    void Update ()
+    {
+
+        eulerAngX = transform.localEulerAngles.x;
+        eulerAngY = transform.localEulerAngles.y;
+        eulerAngZ = transform.localEulerAngles.z;
+
+        if (transform.rotation.z != 0)
+        {
+
+            StartCoroutine("Replace");
+
+            
+        }
+
+        transform.Rotate(Input.GetAxis("Vertical")/1.5f, Input.GetAxis("Horizontal"), -Input.GetAxis("Horizontal") );
+
+		Move ();
+
+		myFieldOfView += myFieldOfView * Time.deltaTime + speed;
+		myFieldOfView = Mathf.Clamp(myFieldOfView, 30.0f, 90.0f);
+		cam.m_Lens.FieldOfView = myFieldOfView;
+
+
+        float terrainHeight = Terrain.activeTerrain.SampleHeight(transform.position); 
+
+        if (terrainHeight > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, terrainHeight, transform.position.z); 
+        }
+
+
+    }
+
+	void Move() {
+
+		transform.position += transform.forward * Time.deltaTime * speed;
+		speed -= transform.forward.y * Time.deltaTime * 4.0f;
+		speed = Mathf.Clamp(speed, 2.0f, 15.0f);
+	}
+
+    IEnumerator Replace ()
+    {
+        yield return new WaitForSeconds(1);
+        print("Done");
+
+        Quaternion target = Quaternion.Euler (eulerAngX, eulerAngY, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+
+        StartCoroutine("Stop");
+    }
+
+    IEnumerator Stop ()
+    {
+        yield return new WaitForSeconds(1);
+
+        StopCoroutine("Replace");
+    }
+
+}
---
+++ Aves Proto/Assets/Scripts/NewCamera.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCamera : MonoBehaviour {
+
+    public float cameraSpeed = 220.0f;
+    public GameObject cameraRotate;
+    Vector3 followPosition;
+    public float clampAngle = 80.0f;
+    public float inputSensivity = 150.0f;
+    public GameObject CameraObj;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    public float rotY = 0.0f;
+    public float rotX = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        float inputX = Input.GetAxis("AxisX");
+        float inputZ = Input.GetAxis("AxisY");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate()
+    {
+            CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = cameraRotate.transform;
+
+        float step = cameraSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
